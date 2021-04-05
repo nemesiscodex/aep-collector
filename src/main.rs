0139f436fe0e8203f12c62aea5fe8b52842f2343
nemesiscodex <nemesiscodex@gmail.com>
2021-04-04 23:45:04 -0400

    Add simple log support

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ mod handler;
 mod middleware;
 mod service;
 
-use actix_web::{App, HttpServer};
+use actix_web::{App, HttpServer, middleware::Logger};
 use crate::middleware::ApiKey;
 use crate::config::Config;
 
@@ -11,6 +11,8 @@ use crate::config::Config;
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     
+    env_logger::init();
+
     let config = Config::from_env();
     let db_pool = config.db_pool().await;
     let key: ApiKey = ApiKey::new(config.api_key);
@@ -19,6 +21,7 @@ async fn main() -> std::io::Result<()> {
 
     HttpServer::new(move || {
         App::new()
+            .wrap(Logger::default())
             .data(db_pool.clone())
             .wrap(key.clone())
             .service(handler::update)
